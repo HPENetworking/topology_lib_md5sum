85c2902f0da833dd745b17882f95d7bb4dbfb5ba
alas <maria.alas@hpe.com>
2016-04-04 14:55:06 -0700

    Adding md5sum_command function

--- lib/topology_lib_md5sum/library.py
+++ lib/topology_lib_md5sum/library.py
@@ -22,35 +22,66 @@ topology_lib_md5sum communication library implementation.
 from __future__ import unicode_literals, absolute_import
 from __future__ import print_function, division
 
+import re
+
 # Add your library functions here.
 
 
-def your_function_here(enode, your_param, shell=None):
+def md5sum_command(enode, _file, t=False, b=False, c=False, status=False,
+                   w=False, quiet=False, strict=False, shell='bash'):
     """
-    Document your function here.
+    This function will execute md5sum command to get a string
+    for compare files
 
-    :param topology.platforms.base.BaseNode enode: Engine node to communicate
-     with.
-    :param bool your_param: This is an example parameter, read the comment
-     below.
-    :param str shell: Shell name to execute commands.
+    -file, this is the file, or directory file to be md5sum
+    -b, --binary         read in binary mode
+    -c, --check          read MD5 sums from the FILEs and check them
+    -t, --text           read in text mode (default)
+
+The following three options are useful only when verifying checksums:
+      --quiet          don't print OK for each successfully verified file
+      --status         don't output anything, status code shows success
+      -w, --warn           warn about improperly formatted checksum lines
+
+      --strict         with --check, exit non-zero for any invalid input
+
     """
     pass
 
     # Usually, the library functions use the parameters to build a command that
     # is to be sent to the enode, for example:
     #
-    # command = 'echo "something"'
-    # if your_param:
-    #     command = '{command} "and something else"'.format(command=command)
+    # command = 'md5sum file.txt'
     #
-    # Then, the enode is used to send the command:
+    # It will return an string
     #
-    # enode('the command to be sent', shell=shell)
+    # enode('md5sum file.txt', shell=shell)
+
+    arguments = locals()
+
+    required_arg = ['file']
+
+    optional_arg = {'t': '-t', 'b': '-b', 'c': '-c', 'status': '--status',
+                    'w': '-w', 'quiet': '--quiet', 'strict': '--strict'}
+
+    options = ''
+
+    for key, value in list(arguments.items()):
+        if value is True:
+            options = '{0}{1} '.format(options, optional_arg.get(key))
+
+    md5sum_cmd = 'md5sum {0}{1}'.format(options, _file)
+    md5sum_response = enode(md5sum_cmd, shell=shell)
+    md5sum_re = (
+      r'(?P<data>\w*)\s+'
+    )
+    return_code = ''
+    re_result = re.match(md5sum_re, md5sum_response)
+    assert re_result
+    result = re_result.groupdict()
+    return result
+
 
 __all__ = [
-    # The Topology framework loads the functions that are in this list to be
-    # used as libraries, so, if you want your function to be loaded, add it
-    # here.
-    'your_function_here'
+    'md5sum_command'
 ]
